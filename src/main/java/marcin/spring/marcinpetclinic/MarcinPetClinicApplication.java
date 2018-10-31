878f35084dd9a2145013ec8ff3863136ecf5a020
ganwik <mw.marcinwilk@gmail.com>
2018-10-31 15:48:04 +0100

    initial project

---
+++ src/main/java/marcin/spring/marcinpetclinic/MarcinPetClinicApplication.java
@@ -0,0 +1,12 @@
+package marcin.spring.marcinpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcinPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarcinPetClinicApplication.class, args);
+    }
+}
