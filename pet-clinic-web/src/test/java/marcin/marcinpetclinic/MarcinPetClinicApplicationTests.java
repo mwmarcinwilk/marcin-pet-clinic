2d87d0964dc4e3d2ec2a948b41e20b3319a04903
ganwik <mw.marcinwilk@gmail.com>
2019-02-18 20:27:27 +0100

    dodanie testow do weba

---
+++ pet-clinic-web/src/test/java/marcin/marcinpetclinic/MarcinPetClinicApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package marcin.marcinpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MarcinPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+*/
