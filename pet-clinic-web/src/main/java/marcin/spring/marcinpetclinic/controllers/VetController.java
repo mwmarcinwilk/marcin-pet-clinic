e778391585b7505655d698d4c88bb276a8b41f5c
Marcin Wilk <marcin.wilk@it-pkp.pl>
2019-02-21 08:06:38 +0100

    vet controller

--- pet-clinic-web/src/main/java/marcin/spring/marcinpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/marcin/spring/marcinpetclinic/controllers/VetController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class indexController {
-    @RequestMapping({"", "/", "index", "templates/index.html"})
-    public String index() {
-        return "index";
+public class VetController {
+    @RequestMapping({"/vets", "/vets/index"})
+    public String listVets() {
+        return "vets/index";
     }
 }
